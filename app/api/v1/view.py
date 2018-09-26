6c0d63a15979524ae399630ce4f165bad3179eeb
DennisMufasa <denny.muasa@gmail.com>
2018-09-26 19:15:12 +0300

    feat(view): sign up new users
    -ensure new users are registred using an endpoint
    -satisfy project requirement
    
    [Start#160492215]

--- app/api/v1/view.py
+++ app/api/v1/view.py
@@ -1,5 +1,5 @@
 """Docstring for endpoints"""
-from flask import request
+from flask import request, make_response
 from .models import Orders
 from . import v1_blue_print
 
@@ -12,3 +12,17 @@ def home():
     cost = request.form['cost']
 
     return Orders().place_order(food, cost)
+
+
+@v1_blue_print.route('/signup', methods=['POST'])
+def update():
+    """sign up new users"""
+    username = request.form['name']
+    email = request.form['email']
+    password = request.form['password']
+    phone = request.form['phone']
+
+    Orders().add_user(username, email, password, phone)
+
+    return make_response(Orders().get_user(username))
+
