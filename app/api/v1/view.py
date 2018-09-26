668f566faa509d9aca73054a5ac4effdec15b05e
DennisMufasa <denny.muasa@gmail.com>
2018-09-27 02:06:12 +0300

    feat(test_views): test place order endpoint
    -ensure the test passes
    -satisfy project requirements
    
    [Start#160795351]

--- app/api/v1/view.py
+++ app/api/v1/view.py
@@ -11,7 +11,7 @@ def home():
     food = request.form['food']
     cost = request.form['cost']
 
-    return Orders().place_order(food, cost)
+    return Orders().place_order(food, cost), 201
 
 
 @v1_blue_print.route('/signup', methods=['POST'])
@@ -24,7 +24,7 @@ def update():
 
     Orders().add_user(username, email, password, phone)
 
-    return make_response(Orders().get_user(username))
+    return make_response(Orders().get_user(username)), 201
 
 
 @v1_blue_print.route('/login', methods=['POST'])
@@ -37,35 +37,35 @@ def login():
     if email == "admin@email.com" and password == "admin":
         return make_response("welcome admin!")
 
-    return Orders().validate_user(email, password)
+    return Orders().validate_user(email, password), 200
 
 
 @v1_blue_print.route('/orders', methods=['GET'])
 def fetch_all_orders():
     """fetch all data"""
-    return Orders().get_orders()
+    return Orders().get_orders(), 200
 
 
 @v1_blue_print.route('/orders/<string:food>', methods=['GET'])
 def fetch_one_order(food):
     """fetch one order based on food"""
-    return Orders().get_one_order(food)
+    return Orders().get_one_order(food), 200
 
 
 @v1_blue_print.route('/update/<string:food>', methods=['PUT'])
 def update_status(food):
     """fetch a specific order and changes any of its values"""
 
-    return Orders().update_order_status(food)
+    return Orders().update_order_status(food), 201
 
 
 @v1_blue_print.route('/show', methods=['GET'])
 def fetch_all():
     """fetch all users"""
-    return Orders().get_all_users()
+    return Orders().get_all_users(), 200
 
 
 @v1_blue_print.route('/show/<string:name>', methods=['GET'])
 def fetch_one(name):
     """fetch a specific user's details based on name"""
-    return Orders().get_user(name)
+    return Orders().get_user(name), 200
--- app/tests/v1/test_views.py
+++ app/tests/v1/test_views.py
@@ -1,16 +1,20 @@
 """Docstring for test_views"""
-# system import
+# system imports
 import os
+import sys
 
-# thirdparty module imports
+base_dir = os.path.abspath(os.path.dirname(__file__))
+sys.path.append(base_dir)
+
+# third party module imports
 import unittest
-import json
 
 # local imports
 from app.api import create_app
+from app.api.v1.models import Orders
 
 
-class Test_views(unittest.TestCase):
+class TestViews(unittest.TestCase):
     """Docstring for Test_views class"""
 
     def setUp(self):
@@ -18,11 +22,29 @@ class Test_views(unittest.TestCase):
 
         self.app = create_app(config_filename='testing')
         self.client = self.app.test_client
+        self.email = 'mufasa@email.com'
+        self.password = 'mfs@18'
         self.food = 'burger'
         self.cost = 200
 
+    def test_signup(self):
+        """Test user registration"""
+        response = self.client().post('/api/v1/signup', data={'email': self.email,
+                                                              'pass': self.password})
+        self.assertEqual(response.status_code, 201)
+
     def test_placeorder_without_loggin(self):
         """Check if the request is possible if not logged on"""
-        resposnse = self.client().post('/api/v1/', data={"food": self.food,
-                                                         "cost": self.cost})
-        self.assertEqual(resposnse.status_code, 201)
+        response = self.client().post('/api/v1/', data={"food": self.food,
+                                                        "cost": self.cost})
+        self.assertNotEqual(response.status_code, 201)
+
+    def test_placeorder(self):
+        """Check if user is able to login"""
+        response = self.client().post('/api/v1/signup', data={"email": self.email,
+                                                              "password": self.password})
+        self.assertEqual(response.status_code, 200)
+
+
+if __name__ == "__main__":
+    unittest.main()
